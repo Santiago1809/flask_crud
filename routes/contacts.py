096cd465cbbcdb91fed7a607fafeef260e9f2b33
Santiuwu1809 <aristizabalsantiago482@gmail.com>
2023-06-01 15:02:54 -0500

    Actualizar usuarios

--- routes/contacts.py
+++ routes/contacts.py
@@ -31,9 +31,17 @@ def about():
     return render_template("about.html")
 
 
-@contacts.route("/update")
-def update():
-    return render_template("update.html")
+@contacts.route("/update/<id>", methods=["POST", "GET"])
+def update(id):
+    contact = Contact.query.get(id)
+    if request.method == "POST":
+        contact.fullname = request.form["fullname"]
+        contact.email = request.form["email"]
+        contact.phone = request.form["phone"]
+
+        db.session.commit()
+        return redirect(url_for('contacts.home'))
+    return render_template("update.html", contact=contact)
 
 
 @contacts.route("/delete/<id>")
