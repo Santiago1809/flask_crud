7ef9fb5e22cc255a65ebc4acc265b9ed26d89883
Santiuwu1809 <aristizabalsantiago482@gmail.com>
2023-05-31 23:07:55 -0500

    Creando rutas y templates

--- routes/contacts.py
+++ routes/contacts.py
@@ -1,13 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 contacts = Blueprint('contacts', __name__)
 
 
 @contacts.route('/')
 def home():
-    return "Lista de contactos"
+    return render_template("index.html")
 
 
 @contacts.route('/new')
 def new():
-    return "saving contact"
+    return render_template("new.html")
+
+
+@contacts.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@contacts.route("/update")
+def update():
+    return render_template("update.html")
+
+
+@contacts.route("/delete")
+def delete():
+    return "delete"
